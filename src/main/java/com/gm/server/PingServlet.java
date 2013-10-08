97d6deb763af15e966539161a3784cd5fa42d498
Wentao Zheng <zhengwt@gmail.com>
2013-10-07 22:06:05 -0400

    add ping and device servlet mapping in web.xml

---
+++ src/main/java/com/gm/server/PingServlet.java
@@ -0,0 +1,20 @@
+package com.gm.server;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PingServlet extends HttpServlet {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    API.ping.execute(req, resp);
+  }
+}
+
